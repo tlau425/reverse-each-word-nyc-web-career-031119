4c24767b5ee722e0d136dd5038354d91ee3e07af
Tommy Lau <tlau425@yahoo.com>
2019-02-27 07:19:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,3 @@
-def reverse_each_word (sentence1)
-  original_array = sentence1.split (" ")
-  new_array = []
-  original_array.each do |sentence1|
-    new_array << sentence1.reverse
-  end
-  new_array.join(" ")
-end
 
 def reverse_each_word (sentence1)
   new_array = sentence1.split (" ")
