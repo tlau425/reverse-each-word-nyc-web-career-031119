e71d5c450832e900be770de53c0518d9db1bf3e1
Tommy Lau <tlau425@yahoo.com>
2019-02-27 07:20:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word (sentence1)
   new_array = sentence1.split (" ")
   test_array = []
   new_array.collect do |sentence1|
-    test_array << sentence2.reverse
+    test_array << sentence1.reverse
   end
   new_array.join(" ")
 end
